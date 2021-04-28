2da0d9449aae5592a5a89d1dbba7085396d49f18
RobMarsh <mrglobe1@gmail.com>
2021-04-28 15:16:57 -0400

    Created array for string equivalents of numbers

--- .idea/main.js
+++ .idea/main.js
@@ -1,3 +1,6 @@
+var phoenetic_numeral_values = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+
+// Read parameters and iterate, skipping first two params
 for (var i = 2; i < process.argv.length; i++)
 {
     console.log(process.argv[i])
