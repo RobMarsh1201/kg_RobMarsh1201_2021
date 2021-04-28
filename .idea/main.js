08dabdb95b332ce411bb0597edc5221e6aec122c
RobMarsh <mrglobe1@gmail.com>
2021-04-28 16:33:32 -0400

    Removed final comma from finalPhoneticString, and added a handful of missing comments.

--- .idea/main.js
+++ .idea/main.js
@@ -4,6 +4,7 @@ const phoneticValue = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Se
 
 //Final string that holds all phoentic parameter equivelents
 let finalPhoenticString = ''
+
 // Read parameters and iterate, skipping first two params
 for (var i = 2; i < process.argv.length; i++)
 {
@@ -13,15 +14,20 @@ for (var i = 2; i < process.argv.length; i++)
     //Split a string into a array of characters
     const splitNumber = process.argv[i].split('');
 
+    //Marches through array of characters, and assigns each character its phoentic value
     for (var k = 0; k < process.argv[i].length; k++)
     {
         // March through array, using the value of each number to call the String numeral array,
-        // and assign its phonetic value from its parseInt
+        // and assign its phonetic value from its parseInt, and adds the phonetic numberal to the workingString variable
         workingString += (phoneticValue[parseInt(splitNumber[k])]);
     }
+    //Adds the string to the final string to be printed
+    finalPhoenticString += workingString
 
-    finalPhoenticString += workingString + ","
-
+    //Handles the comma inbetween main string. Ensures a comma will not be added to the last workingString
+    if (i < process.argv.length - 1)
+        finalPhoenticString += ","
 }
 
+//Prints combined string to console
 console.log(finalPhoenticString);
