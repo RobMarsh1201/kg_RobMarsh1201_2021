166f65bf5e9f066b63646a784619c12ba999cb97
RobMarsh <mrglobe1@gmail.com>
2021-04-28 16:13:12 -0400

    Created Int parameter to phonetic string parser. Need to combine Strings from multiple parameters into one string to be printed.

--- .idea/main.js
+++ .idea/main.js
@@ -1,10 +1,20 @@
-const phoenetic_numeral_values = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+const phoneticValue = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
 
 // Read parameters and iterate, skipping first two params
 for (var i = 2; i < process.argv.length; i++)
 {
-    console.log(process.argv[i]);
+    //Initlaize string to be printed to console
+    let numeralString = ''
+
+    //Split a string into a array of characters
     const splitNumber = process.argv[i].split('');
 
+    for (var k = 0; k < process.argv[i].length; k++)
+    {
+        // March through array, using the value of each number to call the String numeral array,
+        // and assign its phonetic value from its parseInt
+        numeralString += (phoneticValue[parseInt(splitNumber[k])]);
+    }
 
+    console.log(numeralString);
 }
